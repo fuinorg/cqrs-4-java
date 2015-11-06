fee133458c33b894c6ddbf13637d8050fc589e49
Michael Schnell <michael@fuin.org>
2015-11-06 20:43:24 +0100

    Made projector a concrete class

--- src/main/java/org/fuin/cqrs4j/Projector.java
+++ src/main/java/org/fuin/cqrs4j/Projector.java
@@ -17,55 +17,137 @@
  */
 package org.fuin.cqrs4j;
 
-import java.util.Set;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.validation.constraints.NotNull;
 
 import org.fuin.ddd4j.ddd.Event;
 import org.fuin.ddd4j.ddd.EventType;
-import org.fuin.objects4j.common.NotEmpty;
+import org.fuin.esc.api.CommonEvent;
+import org.fuin.esc.api.ReadableEventStoreSync;
+import org.fuin.esc.api.StreamDeletedException;
+import org.fuin.esc.api.StreamEventsSlice;
+import org.fuin.esc.api.StreamId;
+import org.fuin.esc.api.StreamNotFoundException;
+import org.fuin.objects4j.common.Contract;
+import org.fuin.objects4j.common.ContractViolationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
- * Reads events and updates it's view.
+ * Reads events from a stream and calls the appropriate event handlers.
  */
-public interface Projector {
+public final class Projector {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Projector.class);
+
+    private final ReadableEventStoreSync eventStore;
+
+    private final StreamId streamId;
+
+    private final int streamReadPageSize;
+
+    private final ProjectionService projectionService;
+
+    private final Map<EventType, EventHandler> eventHandlers;
 
     /**
-     * Returns a unique identifier for the view.
+     * Constructor with event array.
      * 
-     * @return Unique ID.
+     * @param eventStore
+     *            Event store used for reading the events.
+     * @param streamId
+     *            Unique identifier of the stream the projector reads to update the view.
+     * @param streamReadPageSize
+     *            Page size to read.
+     * @param projectionService
+     *            Used to store the position of the projection.
+     * @param eventHandlers
+     *            Array of event handlers.
      */
-    @NotEmpty
-    public String getViewId();
+    public Projector(@NotNull final ReadableEventStoreSync eventStore, @NotNull final StreamId streamId,
+            final int streamReadPageSize, @NotNull final ProjectionService projectionService,
+            @NotNull final EventHandler... eventHandlers) {
+        this(eventStore, streamId, streamReadPageSize, projectionService, Arrays.asList(eventHandlers));
+    }
 
     /**
-     * Returns a human readable name for the view.
+     * Constructor with event list.
      * 
-     * @return Unique name.
+     * @param eventStore
+     *            Event store used for reading the events.
+     * @param streamId
+     *            Unique identifier of the stream the projector reads to update the view.
+     * @param streamReadPageSize
+     *            Page size to read.
+     * @param projectionService
+     *            Used to store the position of the projection.
+     * @param eventHandlers
+     *            List of event handlers.
      */
-    @NotEmpty
-    public String getViewName();
+    public Projector(@NotNull final ReadableEventStoreSync eventStore, @NotNull final StreamId streamId,
+            final int streamReadPageSize, @NotNull final ProjectionService projectionService,
+            @NotNull final List<EventHandler> eventHandlers) {
+        super();
+        Contract.requireArgNotNull("eventStore", eventStore);
+        Contract.requireArgNotNull("streamId", streamId);
+        Contract.requireArgNotNull("projectionService", projectionService);
+        Contract.requireArgNotNull("eventHandlers", eventHandlers);
+        if (eventHandlers.size() == 0) {
+            throw new ContractViolationException("The argument 'eventHandlers' cannot be an empty list");
+        }
+        this.eventHandlers = new HashMap<>();
+        for (final EventHandler eventHandler : eventHandlers) {
+            if (this.eventHandlers.containsKey(eventHandler.getEventType())) {
+                throw new ContractViolationException(
+                        "The argument 'eventHandlers' contains multiple handlers for event: "
+                                + eventHandler.getEventType());
+            }
+            this.eventHandlers.put(eventHandler.getEventType(), eventHandler);
+        }
+        this.eventStore = eventStore;
+        this.streamId = streamId;
+        this.streamReadPageSize = streamReadPageSize;
+        this.projectionService = projectionService;
+    }
 
     /**
-     * Returns a brief description of what the view is good for.
-     * 
-     * @return Description.
+     * Reads the new events from a stream that has all the events for the view.
      */
-    @NotEmpty
-    public String getViewDescription();
+    public final void readStreamEvents() {
+        LOG.debug("Read stream events: {}", streamId);
 
-    /**
-     * Drops and recreates the view content.
-     */
-    public void rebuildView();
+        final Integer lastEventNumber = projectionService.readProjectionPosition(streamId);
 
-    /**
-     * Returns the event types this projector is interested in.
-     * 
-     * @return Immutable set of events the projector wants to handle.
-     */
-    @NotNull
-    public Set<EventType> getEventTypes();
+        int sliceStart = lastEventNumber;
+        StreamEventsSlice currentSlice;
+        do {
+            final int sliceInc = streamReadPageSize;
+            final int sliceCount = sliceStart + sliceInc;
+            try {
+                LOG.debug("Read slice: streamId={}, sliceStart={}, sliceCount={}", streamId, sliceStart,
+                        sliceCount);
+                currentSlice = eventStore.readEventsForward(streamId, sliceStart, sliceCount);
+                LOG.debug("Result slice: {}", currentSlice);
+            } catch (final StreamNotFoundException ex) {
+                throw new RuntimeException(ex);
+            } catch (final StreamDeletedException ex) {
+                throw new RuntimeException(ex);
+            }
+            if (currentSlice.getEvents().size() > 0) {
+                for (final CommonEvent commonEvent : currentSlice.getEvents()) {
+                    final Event event = (Event) commonEvent.getData();
+                    handle(event);
+                }
+                projectionService.updateProjectionPosition(streamId, currentSlice.getNextEventNumber());
+            }
+            sliceStart = currentSlice.getNextEventNumber();
+        } while (!currentSlice.isEndOfStream());
+
+    }
 
     /**
      * Handles the given event. Events the projector is not interested in are ignored.
@@ -73,6 +155,11 @@ public interface Projector {
      * @param event
      *            Event to apply to the view.
      */
-    public void handle(@NotNull Event event);
+    private void handle(final Event event) {
+        final EventHandler handler = eventHandlers.get(event.getEventType());
+        if (handler != null) {
+            handler.handle(event);
+        }
+    }
 
 }
