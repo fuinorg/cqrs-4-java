a136ff67a5f6f987673787fa1787145cfec3083d
Michael Schnell <michael@fuin.org>
2016-07-11 15:40:50 +0200

    Fixed mising constructors

--- src/main/java/org/fuin/cqrs4j/AbstractCommand.java
+++ src/main/java/org/fuin/cqrs4j/AbstractCommand.java
@@ -17,7 +17,12 @@
  */
 package org.fuin.cqrs4j;
 
+import javax.validation.constraints.NotNull;
+
 import org.fuin.ddd4j.ddd.AbstractEvent;
+import org.fuin.ddd4j.ddd.Event;
+import org.fuin.ddd4j.ddd.EventId;
+import org.fuin.objects4j.common.Nullable;
 
 /**
  * Base class for all commands.
@@ -25,5 +30,35 @@ import org.fuin.ddd4j.ddd.AbstractEvent;
 public abstract class AbstractCommand extends AbstractEvent implements Command {
 
     private static final long serialVersionUID = 1000L;
+
+    /**
+     * Default constructor.
+     */
+    public AbstractCommand() {
+        super();
+    }
+
+    /**
+     * Constructor with event this one responds to. Convenience method to set the correlation and causation
+     * identifiers correctly.
+     * 
+     * @param respondTo
+     *            Causing event.
+     */
+    public AbstractCommand(@NotNull final Event respondTo) {
+        super(respondTo);
+    }
+
+    /**
+     * Constructor with optional data.
+     * 
+     * @param correlationId
+     *            Correlation ID.
+     * @param causationId
+     *            ID of the event that caused this one.
+     */
+    public AbstractCommand(@Nullable final EventId correlationId, @Nullable final EventId causationId) {
+        super(correlationId, causationId);
+    }
 
 }
