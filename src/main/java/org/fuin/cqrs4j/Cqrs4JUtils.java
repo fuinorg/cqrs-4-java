2b40294f7b99525d2f0951688d130124819da3d7
Michael Schnell <michael@fuin.org>
2019-05-17 17:07:14 +0200

    Added specialized interface and base impl for domain commands

--- src/main/java/org/fuin/cqrs4j/Cqrs4JUtils.java
+++ src/main/java/org/fuin/cqrs4j/Cqrs4JUtils.java
@@ -20,10 +20,14 @@ package org.fuin.cqrs4j;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.Semaphore;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
 import javax.validation.constraints.NotNull;
 
+import org.fuin.ddd4j.ddd.AggregateRootId;
 import org.fuin.objects4j.common.ConstraintViolationException;
 import org.fuin.objects4j.common.Contract;
 import org.slf4j.Logger;
@@ -39,6 +43,12 @@ public final class Cqrs4JUtils {
             .unmodifiableList(Arrays.asList(Result.class, ConstraintViolationException.class));
 
     private static final Logger LOG = LoggerFactory.getLogger(Cqrs4JUtils.class);
+
+    /** Some constraints were violated. */
+    public static final String PRECONDITION_VIOLATED = "PRECONDITION_VIOLATED";
+
+    /** Result code for {@link #verifyParamIdEqualsCmdAggregateId(AggregateRootId, DomainCommand)} failures. */
+    public static final String PARAM_ID_NOT_EQUAL_CMD_AGGREGATE_ID = "PARAM_ID_NOT_EQUAL_CMD_AGGREGATE_ID";
 
     /**
      * Private by intention.
@@ -88,6 +98,62 @@ public final class Cqrs4JUtils {
         } catch (final InterruptedException ex) { // NOSONAR
             LOG.warn("Couldn't clear view", ex);
         }
+    }
+
+    /**
+     * Verifies a precondition. In case of constraint violations, the error is logged and an error result is returned.
+     * 
+     * @param validator
+     *            Validator to use.
+     * @param obj
+     *            Object to validate. A {@literal null} value is considered to be an error.
+     * 
+     * @return Error result or {@literal null} if validation was successful.
+     */
+    public static Result<?> verifyPrecondition(@NotNull final Validator validator, @NotNull final Object obj) {
+
+        Contract.requireArgNotNull("validator", validator);
+        Contract.requireArgNotNull("obj", obj);
+
+        final Set<ConstraintViolation<Object>> violations = validator.validate(obj);
+        if (violations.isEmpty()) {
+            return null;
+        }
+        final String errors = Contract.asString(violations, ", ");
+        LOG.error(errors);
+        return Result.error(PRECONDITION_VIOLATED, errors);
+    }
+
+    /**
+     * Verifies that an aggregate identifier from a parameter is equal to the aggregate identifier from the command. This is helpful, if for
+     * example the URL contains the name of the aggregate, followed by an aggregate identifier.<br>
+     * Example: POST /customer/f832a5a4-dd80-49df-856a-7274de82cd6b/create<br>
+     * The ID from the URL must match the aggregate ID that is passed via the command in the body.
+     * 
+     * @param aggregateRootId
+     *            Identifier to compare with.
+     * @param cmd
+     *            Command that has the aggregate ID to compare with.
+     * 
+     * @return Error result or {@literal null} if validation was successful.
+     * 
+     * @param <ID>
+     *            Type of the aggregate root identifier.
+     */
+    public static <ID extends AggregateRootId> Result<?> verifyParamIdEqualsCmdAggregateId(@NotNull final ID aggregateRootId,
+            @NotNull final DomainCommand<ID> cmd) {
+
+        Contract.requireArgNotNull("aggregateRootId", aggregateRootId);
+        Contract.requireArgNotNull("cmd", cmd);
+
+        if (aggregateRootId.equals(cmd.getAggregateRootId())) {
+            return null;
+        }
+        final String msg = "URL parameter ID '" + aggregateRootId + "' is not the same as command's aggregate root ID: '"
+                + cmd.getAggregateRootId() + "'";
+        LOG.error(msg + " [" + PARAM_ID_NOT_EQUAL_CMD_AGGREGATE_ID + "]");
+        return Result.error(PARAM_ID_NOT_EQUAL_CMD_AGGREGATE_ID, msg);
+
     }
 
 }
--- src/test/java/org/fuin/cqrs4j/Cqrs4JUtilsTest.java
+++ src/test/java/org/fuin/cqrs4j/Cqrs4JUtilsTest.java
@@ -17,13 +17,73 @@
  */
 package org.fuin.cqrs4j;
 
-import org.fuin.units4j.TestOmitted;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import org.fuin.ddd4j.ddd.EventType;
+import org.fuin.objects4j.common.Contract;
+import org.fuin.objects4j.vo.EmailAddressStr;
+import org.junit.Test;
 
 // CHECKSTYLE:OFF
-@TestOmitted("TODO Implement!")
 public final class Cqrs4JUtilsTest {
 
-    // TODO Implement tests!
+    @Test
+    public void testVerifyPrecondition() {
+
+        final MyClass myObj1 = new MyClass();
+        assertThat(Cqrs4JUtils.verifyPrecondition(Contract.getValidator(), myObj1)).isEqualTo(new Result<>(ResultType.ERROR,
+                Cqrs4JUtils.PRECONDITION_VIOLATED, "MyClass.email must not be null, MyClass.name must not be empty", null));
+
+        final MyClass myObj2 = new MyClass();
+        myObj2.email = "test@fuin.org";
+        myObj2.name = "test";
+        assertThat(Cqrs4JUtils.verifyPrecondition(Contract.getValidator(), myObj2)).isNull();
+
+    }
+
+    @Test
+    public void testVerifyParamIdEqualsCmdAggregateId() {
+
+        // PREPARE
+        AId aid1 = new AId(1L);
+        AId aid2 = new AId(2L);
+        final DomainCommand<AId> cmd = new AbstractDomainCommand<AId>() {
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            @NotNull
+            public AId getAggregateRootId() {
+                return aid1;
+            }
+
+            @Override
+            @NotNull
+            public EventType getEventType() {
+                return new EventType("MyCommand");
+            }
+        };
+
+        // TEST & VERIFY
+        assertThat(Cqrs4JUtils.verifyParamIdEqualsCmdAggregateId(aid1, cmd)).isNull();
+        assertThat(Cqrs4JUtils.verifyParamIdEqualsCmdAggregateId(aid2, cmd))
+                .isEqualTo(new Result<>(ResultType.ERROR, Cqrs4JUtils.PARAM_ID_NOT_EQUAL_CMD_AGGREGATE_ID,
+                        "URL parameter ID 'AId [id=2]' is not the same as command's aggregate root ID: 'AId [id=1]'", null));
+
+    }
+
+    private static class MyClass {
+
+        @NotEmpty
+        private String name;
+
+        @NotNull
+        @EmailAddressStr
+        private String email;
+
+    }
 
 }
 // CHECKSTYLE:ON
